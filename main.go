f5b4108d894795d3bab8e29cb45c20087b2c7a2a
Alan <alaeduardosprea26@gmail.com>
2025-09-09 15:06:34 -0300

    feat: add logger

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
-import "github.com/Alansprea/gopportunities/router"
+import (
+	"github.com/Alansprea/gopportunities/config"
+	"github.com/Alansprea/gopportunities/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
 
+	logger = *config.GetLogger(("main"))
+	// Inicialize Conifgs
+
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config initialization error : v%", err)
+		return
+	}
 	// Inicialize Router
 	router.Inicialize()
 }
